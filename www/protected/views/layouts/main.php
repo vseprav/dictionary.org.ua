3584f2ee06d485d7e06ab6123e92b5e96db79c6c
vseprav <vseprav@gmail.com>
2013-03-24 23:49:11 +0200

    fix main menu

--- www/protected/views/layouts/main.php
+++ www/protected/views/layouts/main.php
@@ -24,7 +24,7 @@
 			<?php $this->widget('zii.widgets.CMenu',array(
 				'items'=>array(
 					array('label'=>'Home', 'url'=>array('/site/index')),
-					array('label'=>'About', 'url'=>array('/site/page')),
+					array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 					array('label'=>'Dictionaries', 'url'=>array('/dictionaries')),
 					array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 					array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
